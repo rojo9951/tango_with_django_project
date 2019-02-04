9e6191c945fab1ecb47511549d5596c20c534379
Ross Johnstone <2325563j@student.gla.ac.uk>
2019-02-04 19:29:55 +0000

    5

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -15,6 +15,9 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -64,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+				'django.template.context_processors.media'
             ],
         },
     },
@@ -119,4 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATICFILES_DIRS = [STATIC_DIR, ]
 STATIC_URL = '/static/'
+
+#Media files
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -17,13 +17,16 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     #maps basic URL to the index view in the rango app
     url(r'^$', views.index, name='index'),
     url(r'rango/', include('rango.urls')),
+	url(r'^about', views.about, name='about'),
     # above maps any URLS starting
     # with rango/ to be handled by
     # the rango application
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
